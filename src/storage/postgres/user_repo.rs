6d82674535748d181c6a1cdb913b11b584f3d2fe
antonguzun <ayuguzun@gmail.com>
2022-03-31 21:48:52 +0300

    dry user repo

--- src/storage/postgres/user_repo.rs
+++ src/storage/postgres/user_repo.rs
@@ -5,8 +5,9 @@ use crate::usecases::user::get_user::{FindUserById, RemoveUserById};
 use crate::usecases::user::user_creator::CreateUser;
 use async_trait::async_trait;
 use chrono;
-use deadpool_postgres::Pool;
+use deadpool_postgres::{Client, Pool};
 use log::error;
+use tokio_postgres::{Row, Statement};
 
 pub struct UserRepo {
     db_pool: Pool,
@@ -30,24 +31,37 @@ const INSERT_USER_QUERY: &str = "INSERT INTO users
 const FIND_USER_BY_VERIFICATION: &str =
     "SELECT user_id FROM users WHERE username=$1 AND password_hash=$2 AND is_deleted=FALSE";
 
+async fn get_client(db_pool: &Pool) -> Result<Client, AccessModelError> {
+    match db_pool.get().await {
+        Ok(client) => Ok(client),
+        Err(e) => {
+            error!("{}", e);
+            Err(AccessModelError::TemporaryError)
+        }
+    }
+}
+
+async fn prepare_stmt(client: &Client, query: &str) -> Result<Statement, AccessModelError> {
+    match client.prepare(query).await {
+        Ok(stmt) => Ok(stmt),
+        Err(e) => {
+            error!("{}", e);
+            Err(AccessModelError::FatalError)
+        }
+    }
+}
+
+impl User {
+    fn from_sql_result(row: &Row) -> User {
+        User::new(row.get(0), row.get(1), row.get(2), row.get(3), row.get(4))
+    }
+}
 #[async_trait]
 impl FindUserById for UserRepo {
     async fn find_user_by_id(&self, user_id: i32) -> Result<User, AccessModelError> {
-        let client = match self.db_pool.get().await {
-            Ok(client) => client,
-            Err(e) => {
-                error!("{}", e);
-                return Err(AccessModelError::TemporaryError);
-            }
-        };
+        let client = get_client(&self.db_pool).await?;
+        let stmt = prepare_stmt(&client, GET_BY_ID_QUERY).await?;
 
-        let stmt = match client.prepare(GET_BY_ID_QUERY).await {
-            Ok(stmt) => stmt,
-            Err(e) => {
-                error!("{}", e);
-                return Err(AccessModelError::FatalError);
-            }
-        };
         let rows = match client.query(&stmt, &[&user_id]).await {
             Ok(rows) => rows,
             Err(e) => {
@@ -57,13 +71,7 @@ impl FindUserById for UserRepo {
         };
         match rows.len() {
             0 => Err(AccessModelError::NotFoundError),
-            _ => Ok(User::new(
-                rows[0].get(0),
-                rows[0].get(1),
-                rows[0].get(2),
-                rows[0].get(3),
-                rows[0].get(4),
-            )),
+            _ => Ok(User::from_sql_result(&rows[0])),
         }
     }
 }
@@ -71,52 +79,29 @@ impl FindUserById for UserRepo {
 #[async_trait]
 impl RemoveUserById for UserRepo {
     async fn remove_user_by_id(&self, user_id: i32) -> Result<(), AccessModelError> {
-        let client = match self.db_pool.get().await {
-            Ok(client) => client,
-            Err(e) => {
-                error!("{}", e);
-                return Err(AccessModelError::TemporaryError);
-            }
-        };
-        let stmt = match client.prepare(DELETE_BY_ID_QUERY).await {
-            Ok(stmt) => stmt,
-            Err(e) => {
-                error!("{}", e);
-                return Err(AccessModelError::FatalError);
-            }
-        };
+        let client = get_client(&self.db_pool).await?;
+        let stmt = prepare_stmt(&client, DELETE_BY_ID_QUERY).await?;
+
         let now = chrono::Utc::now();
-        let res = match client.execute(&stmt, &[&now, &user_id]).await {
-            Ok(res) if res == 0 => return Err(AccessModelError::NotFoundError),
-            Ok(_) => Ok(()),
+        match client.execute(&stmt, &[&now, &user_id]).await {
+            Ok(res) if res != 0 => Ok(()),
+            Ok(_) => Err(AccessModelError::NotFoundError),
             Err(e) => {
                 error!("{}", e);
-                return Err(AccessModelError::FatalError);
+                Err(AccessModelError::FatalError)
             }
-        };
-        res
+        }
     }
 }
 
 #[async_trait]
 impl CreateUser for UserRepo {
     async fn save_user_in_storage(&self, user: UserForCreation) -> Result<User, AccessModelError> {
-        let client = match self.db_pool.get().await {
-            Ok(client) => client,
-            Err(e) => {
-                error!("{}", e);
-                return Err(AccessModelError::TemporaryError);
-            }
-        };
-        let stmt = match client.prepare(INSERT_USER_QUERY).await {
-            Ok(stmt) => stmt,
-            Err(e) => {
-                error!("{}", e);
-                return Err(AccessModelError::FatalError);
-            }
-        };
+        let client = get_client(&self.db_pool).await?;
+        let stmt = prepare_stmt(&client, INSERT_USER_QUERY).await?;
+
         let now = chrono::Utc::now();
-        let user = match client
+        match client
             .query(
                 &stmt,
                 &[
@@ -130,22 +115,16 @@ impl CreateUser for UserRepo {
             )
             .await
         {
-            Ok(rows) if rows.len() == 1 => User::new(
-                rows[0].get(0),
-                rows[0].get(1),
-                rows[0].get(2),
-                rows[0].get(3),
-                rows[0].get(4),
-            ),
+            Ok(rows) if rows.len() == 1 => Ok(User::from_sql_result(&rows[0])),
+            Ok(_) => {
+                error!("During creation user got count of retirning rows not equals one");
+                Err(AccessModelError::FatalError)
+            }
             Err(e) => {
                 error!("{}", e);
-                return Err(AccessModelError::FatalError);
+                Err(AccessModelError::FatalError)
             }
-            Ok(_) => {
-                return Err(AccessModelError::FatalError);
-            }
-        };
-        Ok(user)
+        }
     }
 }
 
@@ -156,31 +135,16 @@ impl SignInVerification for UserRepo {
         username: &str,
         hash: &str,
     ) -> Result<i32, AccessModelError> {
-        let client = match self.db_pool.get().await {
-            Ok(client) => client,
-            Err(e) => {
-                error!("{}", e);
-                return Err(AccessModelError::TemporaryError);
-            }
-        };
+        let client = get_client(&self.db_pool).await?;
+        let stmt = prepare_stmt(&client, FIND_USER_BY_VERIFICATION).await?;
 
-        let stmt = match client.prepare(FIND_USER_BY_VERIFICATION).await {
-            Ok(stmt) => stmt,
+        match client.query(&stmt, &[&username, &hash]).await {
+            Ok(rows) if rows.len() != 0 => Ok(rows[0].get(0)),
+            Ok(_) => Err(AccessModelError::NotFoundError),
             Err(e) => {
                 error!("{}", e);
-                return Err(AccessModelError::FatalError);
+                Err(AccessModelError::FatalError)
             }
-        };
-        let rows = match client.query(&stmt, &[&username, &hash]).await {
-            Ok(rows) => rows,
-            Err(e) => {
-                error!("{}", e);
-                return Err(AccessModelError::FatalError);
-            }
-        };
-        match rows.len() {
-            0 => Err(AccessModelError::NotFoundError),
-            _ => Ok(rows[0].get(0)),
         }
     }
 }
