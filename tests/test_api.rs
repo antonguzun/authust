8d2b6d976c5d3be5c5c15a80f03d5ac91873702b
antonguzun <ayuguzun@gmail.com>
2022-03-31 12:02:34 +0300

    refactoring tests

--- tests/test_api.rs
+++ tests/test_api.rs
@@ -1,55 +1,11 @@
-use actix_http::Request;
-use actix_web::body::BoxBody;
-use actix_web::dev::{Service, ServiceResponse};
-use actix_web::{http::header, test, web, App};
-use rust_crud::apps::init_api_v1;
-use rust_crud::common::{Config, Resources};
+use actix_web::{http::header, test};
 use rust_crud::usecases::user::entities::{SingnedInfo, User};
 use serde_json::json;
-use std::fs;
 
+mod utils;
+use utils::init_test_service;
 mod constants;
-
-async fn refresh_db(resources: &Resources) -> () {
-    let client = resources.db_pool.get().await.unwrap();
-
-    let migration_paths = fs::read_dir("./tests/migrations").unwrap();
-
-    for path in migration_paths {
-        let filename = path.unwrap().path().display().to_string();
-        let query = &fs::read_to_string(&filename).unwrap();
-        client.query(query, &[]).await.unwrap();
-    }
-
-    client.query("TRUNCATE TABLE users;", &[]).await.unwrap();
-    client
-        .query(
-            "INSERT INTO users 
-        (user_id, username, password_hash, enabled, created_at, updated_at, is_deleted)
-        VALUES 
-        (1, 'Ivan', '1234', TRUE, '2016-06-22 22:10:25+03', '2016-06-22 22:10:25+03', FALSE), 
-        (2, 'Anton', $1, TRUE, '2022-06-22 22:10:25+00', '2022-06-22 22:10:25+00', FALSE), 
-        (3, 'Godzilla', '1234', TRUE, '2022-06-22 22:10:25+00', '2022-06-22 22:10:25+00', FALSE)
-        ON CONFLICT DO NOTHING;",
-            &[&constants::TEST_PASSWORD_HASH],
-        )
-        .await
-        .unwrap();
-}
-
-async fn init_test_service(
-) -> impl Service<Request, Response = ServiceResponse<BoxBody>, Error = actix_web::Error> {
-    let config = Config::create_config();
-    let resources = Resources::create_resources(&config).await;
-    refresh_db(&resources).await;
-    test::init_service(
-        App::new()
-            .app_data(config.clone())
-            .data(resources.clone())
-            .service(web::scope("/api/v1").configure(init_api_v1)),
-    )
-    .await
-}
+use constants::TEST_PASSWORD;
 
 #[actix_web::test]
 async fn test_get_user() {
@@ -76,7 +32,6 @@ async fn test_get_user_wrong_params() {
         .uri("/api/v1/user/sadf")
         .to_request();
     let resp = test::call_service(&mut app, req).await;
-    // странно что web::Path приводит к 404 ошибке, а не к 400
     assert_eq!(resp.status(), 404);
 }
 
@@ -149,7 +104,7 @@ async fn test_sign_in() {
     let mut app = init_test_service().await;
     let request_body = json!({
         "username": "Anton",
-        "password": constants::TEST_PASSWORD,
+        "password": TEST_PASSWORD,
     });
     let req = test::TestRequest::post()
         .insert_header(header::ContentType::json())
---
+++ tests/utils.rs
@@ -0,0 +1,51 @@
+use actix_http::Request;
+use actix_web::body::BoxBody;
+use actix_web::dev::{Service, ServiceResponse};
+use actix_web::{test, web, App};
+use rust_crud::apps::init_api_v1;
+use rust_crud::common::{Config, Resources};
+use std::fs;
+
+#[path = "./constants.rs"]
+mod constants;
+
+async fn refresh_db(resources: &Resources) -> () {
+    let client = resources.db_pool.get().await.unwrap();
+
+    let migration_paths = fs::read_dir("./tests/migrations").unwrap();
+
+    for path in migration_paths {
+        let filename = path.unwrap().path().display().to_string();
+        let query = &fs::read_to_string(&filename).unwrap();
+        client.query(query, &[]).await.unwrap();
+    }
+
+    client.query("TRUNCATE TABLE users;", &[]).await.unwrap();
+    client
+        .query(
+            "INSERT INTO users 
+        (user_id, username, password_hash, enabled, created_at, updated_at, is_deleted)
+        VALUES 
+        (1, 'Ivan', '1234', TRUE, '2016-06-22 22:10:25+03', '2016-06-22 22:10:25+03', FALSE), 
+        (2, 'Anton', $1, TRUE, '2022-06-22 22:10:25+00', '2022-06-22 22:10:25+00', FALSE), 
+        (3, 'Godzilla', '1234', TRUE, '2022-06-22 22:10:25+00', '2022-06-22 22:10:25+00', FALSE)
+        ON CONFLICT DO NOTHING;",
+            &[&constants::TEST_PASSWORD_HASH],
+        )
+        .await
+        .unwrap();
+}
+
+pub async fn init_test_service(
+) -> impl Service<Request, Response = ServiceResponse<BoxBody>, Error = actix_web::Error> {
+    let config = Config::create_config();
+    let resources = Resources::create_resources(&config).await;
+    refresh_db(&resources).await;
+    test::init_service(
+        App::new()
+            .app_data(config.clone())
+            .data(resources.clone())
+            .service(web::scope("/api/v1").configure(init_api_v1)),
+    )
+    .await
+}
