96bf988a5a0733323ee99a9efe60e99a5ab65088
antonguzun <ayuguzun@gmail.com>
2022-04-09 17:58:55 +0300

    make system db simple

--- src/storage/postgres/system.rs
+++ src/storage/postgres/system.rs
@@ -1,14 +1,11 @@
-use crate::storage::postgres::base::{get_client, prepare_stmt};
+use crate::storage::postgres::base::get_client;
 use crate::usecases::base_entities::AccessModelError;
 use deadpool_postgres::Pool;
 use log::error;
 
-const READY_QUERY: &str = "SELECT 1";
-
 pub async fn check_db(db_pool: &Pool) -> Result<(), AccessModelError> {
     let client = get_client(db_pool).await?;
-    let stmt = prepare_stmt(&client, READY_QUERY).await?;
-    match client.query(&stmt, &[]).await {
+    match client.simple_query("SELECT 1").await {
         Ok(_) => Ok(()),
         Err(e) => {
             error!("{}", e);
